45a2f906a8179b1dbc4a8e264998bdbd184ac5b3
koesnij <jinseok@moonzzinMBP.local>
2020-08-28 17:49:46 +0900

    #4.3 Apollo Client

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { ApolloProvider } from 'react-apollo-hooks';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../Styles/GlobalStyles';
+import Client from '../Apollo/Client';
 import Theme from '../Styles/Theme';
 import Router from './Router';
 
 export default () => (
   <ThemeProvider theme={Theme}>
-    <>
+    <ApolloProvider client={Client}>
       <GlobalStyles />
       <Router isLoggedIn={false} />
-    </>
+    </ApolloProvider>
   </ThemeProvider>
 );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
+import Client from './Apollo/Client';
+import { ApolloProvider } from 'react-apollo';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={Client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
