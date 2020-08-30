e9d88d65e136602fda12147bbbdd5bdf25b1c3f1
koesnij <jinseok@moonzzinMBP.local>
2020-08-30 20:40:58 +0900

    #6.2 Header Logic II

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -73,7 +73,8 @@ const ME = gql`
 
 export default withRouter(({ history }) => {
   const search = useInput('');
-  const meQuery = useQuery(ME);
+  const { data } = useQuery(ME);
+  console.log(data);
   const onSearchSubmit = (e) => {
     e.preventDefault();
     history.push(`/search?term=${search.value}`);
@@ -98,9 +99,15 @@ export default withRouter(({ history }) => {
           <HeaderLink to="/notifications">
             <HeartEmpty />
           </HeaderLink>
-          <HeaderLink to="/username">
-            <User />
-          </HeaderLink>
+          {data ? (
+            <HeaderLink to={`${data.me.username}`}>
+              <User />
+            </HeaderLink>
+          ) : (
+            <HeaderLink to="/#">
+              <User />
+            </HeaderLink>
+          )}
         </HeaderColumn>
       </HeaderWrapper>
     </Header>
