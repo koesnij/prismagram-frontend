981366bb71fe48914a58ccc23f820d89400af5b8
koesnij <jinseok@moonzzinMBP.local>
2020-08-31 15:52:09 +0900

    #7.0 Getting the Feed and Loader Component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -74,7 +74,6 @@ const ME = gql`
 export default withRouter(({ history }) => {
   const search = useInput('');
   const { data } = useQuery(ME);
-  console.log(data);
   const onSearchSubmit = (e) => {
     e.preventDefault();
     history.push(`/search?term=${search.value}`);
--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,1 +1,46 @@
-export default () => 'Feed';
+import React from 'react';
+import styled from 'styled-components';
+import { gql, useQuery } from '@apollo/react-hooks';
+import Loader from '../Components/Loader';
+
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        username
+      }
+      files {
+        id
+        url
+      }
+      isLiked
+      likeCount
+      comments {
+        id
+        text
+        user {
+          id
+          avatar
+          username
+        }
+      }
+    }
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 60vh;
+`;
+
+export default () => {
+  const { data, loading } = useQuery(FEED_QUERY);
+  return <Wrapper>{loading && <Loader />}</Wrapper>;
+};
